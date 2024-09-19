d1aca02cf635337cb12533eff6dac6bed3e91c90
Thomas Harvey <thomasharvey1415@gmail.com>
2024-09-19 15:15:55 +0100

    Uploading initial files

---
+++ client/main.js
@@ -0,0 +1,18 @@
+//TODO DOM manipulation.
+
+//TODO Select form and feedback container.
+
+//TODO Form: Event to submit form data.
+
+//! When I finish the project, DO NOT forget to replace local host url with the deployed url.
+//! We need to fetch the create endpoint to send form values to the server.
+// fetch("localhost-url/endpoint"),{
+//   method:
+//   headers:
+//   body:
+// }
+
+//TODO FEEDBACK CONTAINER.
+//TODO Fetch the READ endpoint to access the data.
+
+//TODO Create DOM elements to contain the data.
---
+++ server/server.js
@@ -0,0 +1,9 @@
+//TODO Import express, cors, pg, dotnev.
+
+//TODO Initialise express, configure dotenv, tell express to use json, cors.
+
+//TODO Set up database pool with values from .env file.
+
+//TODO Set up port to listen and root route.
+
+//TODO Create READ and CREATE route, use SQL to do what we need.
