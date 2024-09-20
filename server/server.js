c8fb93e2af624010c4824be4821281ca72dd614d
Thomas Harvey <thomasharvey1415@gmail.com>
2024-09-20 14:10:45 +0100

    Setting up the server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,32 @@
 //TODO Import express, cors, pg, dotnev.
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
 
 //TODO Initialise express, configure dotenv, tell express to use json, cors.
-
+const app = express();
+app.use(express.json());
+app.use(cors());
 //TODO Set up database pool with values from .env file.
-
+dotenv.config();
+const dbConnectionString = process.env.DATABASE_URL;
+export const db = new pg.Pool({
+  connectionString: dbConnectionString,
+});
 //TODO Set up port to listen and root route.
-
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`App listening at PORT ${PORT}`);
+});
+app.get("/", (request, response) => {
+  response.json({ message: "You are currently at the root route" });
+});
 //TODO Create READ and CREATE route, use SQL to do what we need.
+app.get("/get-data", (request, response) => {
+  response.json({ message: ` Recieving data from the database` });
+});
+
+app.post("/send-data", (request, response) => {
+  response.json({ message: `Sending data to the database` });
+});
