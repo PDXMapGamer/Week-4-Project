5a426fc5e41d172dd79c18f6b2ca633089329f5e
Thomas Harvey <thomasharvey1415@gmail.com>
2024-09-21 13:33:31 +0100

    Seeded dummy data into the database

--- server/seed.js
+++ server/seed.js
@@ -1,1 +1,15 @@
-//TODO  add the SQL and seed.
+//TODO  Add the SQL and seed.
+//TODO Seed dummy data
+import { db } from "./server.js";
+
+db.query(`CREATE TABLE IF NOT EXISTS guests(
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    email TEXT,
+    favourite_animal VARCHAR(42),
+    feedback TEXT
+);`);
+
+db.query(`INSERT INTO guests(username, email, favourite_animal, feedback)
+VALUES ('Dummy_1', 'realmail@mail.com', 'Dog', 'This is a sample test to test features'),
+('Dummy_2', 'fakemail@mail.com', 'Cat', 'lorem 50')`);
